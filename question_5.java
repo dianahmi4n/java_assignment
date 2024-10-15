d90e2486f83fd60c9a91606a7fcdfdac3774bf84
Dianah Mian <152776323+dianahmi4n@users.noreply.github.com>
2024-10-15 11:29:38 +0300

    Update question_5.java

--- question_5.java
+++ question_5.java
@@ -1,1 +1,20 @@
+public class question_5{
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        // Integer array
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        System.out.println("Integer Array:");
+        printArray(intArray);
+
+        // String array
+        String[] strArray = {"Apple", "Banana", "Cherry"};
+        System.out.println("String Array:");
+        printArray(strArray);
+    }
+}
